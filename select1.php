13a40d5ed12116d24c11a98f3d6337228669f2f6
gideaoms <gideaoms@gmail.com>
2016-01-15 09:10:24 -0200

    Create select1.php

---
+++ select1.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Projeto Select</title>
+		<link href="//fonts.googleapis.com/css?family=Open+Sans:400,700&amp;subset=latin,latin-ext,cyrillic" rel="stylesheet" type="text/css">
+		<style type="text/css">
+			html {
+				font-family: "Open Sans","Helvetica Neue",Arial,Helvetica,sans-serif
+			}
+
+			.input-select {
+				padding: 5px 10px;
+				border: 1px solid b3b3b3;
+				background-image: url(select-arrow.png);
+				background-color: #fff;
+				background-repeat: no-repeat;
+				background-position: right 50%;
+				background-size: 20px 10px;
+				padding-right: 25px;
+				-webkit-appearance: none;
+				-moz-appearance: none;
+				appearance: none;
+			}
+		</style>
+	</head>
+	<body>
+		<select class="input-select">
+			<option>Option 1</option>
+			<option>Option 2</option>
+			<option>Option 3</option>
+			<option>Option 4</option>
+			<option>Option 5</option>
+		</select>
+	</body>
+</html>
